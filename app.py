1f89aef043bb8b0604adad5eb53e11ca6a83ed72
cameron-green-1 <cameron.green@voxelstudio.co.uk>
2024-04-03 14:06:17 +0100

    Swap to SDXL

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 import os
 import torch
-from diffusers import StableDiffusionXLPipeline
+from diffusers import StableDiffusionXLPipeline, UNet2DConditionModel, EulerDiscreteScheduler
+from huggingface_hub import hf_hub_download
+from safetensors.torch import load_file
 from flask import Flask, request
 
-model_id = "stabilityai/stable-diffusion-xl-base-1.0"
-pipe = StableDiffusionXLPipeline.from_pretrained(model_id, torch_dtype=torch.float16).to("cuda")
+base = "stabilityai/stable-diffusion-xl-base-1.0"
+repo = "ByteDance/SDXL-Lightning"
+ckpt = "sdxl_lightning_2step_unet.safetensors"
+
+# Load model.
+unet = UNet2DConditionModel.from_config(base, subfolder="unet").to("cuda", torch.float16)
+unet.load_state_dict(load_file(hf_hub_download(repo, ckpt), device="cuda"))
+pipe = StableDiffusionXLPipeline.from_pretrained(base, unet=unet, torch_dtype=torch.float16, variant="fp16").to("cuda")
+
+# Ensure sampler uses "trailing" timesteps.
+pipe.scheduler = EulerDiscreteScheduler.from_config(pipe.scheduler.config, timestep_spacing="trailing")
 
 app = Flask(__name__)
 
@@ -21,6 +32,8 @@ def generate_image():
         sd_folder = os.path.join(documents_path, 'sd')
         os.makedirs(sd_folder, exist_ok=True)  
         image = pipe(prompt).images[0]
+        # Ensure using the same inference steps as the loaded model and CFG set to 0.
+        image = pipe(prompt, num_inference_steps=4, guidance_scale=0).images[0]
         image_filename = "generated_image.png"
         image_path = os.path.join(sd_folder, image_filename)  
         image.save(image_path)
